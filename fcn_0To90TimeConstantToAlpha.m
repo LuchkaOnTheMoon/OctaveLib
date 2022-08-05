5b44b2b06f2d05309c52711230b16aebb488afdb
LuchkaOnTheMoon <luchka.luconi@gmail.com>
2022-08-05 09:10:25 +0200

    Adding exponential smoothing function, and a set of three different smoothing time constant calculation functions calculating it according to different definitions.

---
+++ fcn_0To90TimeConstantToAlpha.m
@@ -0,0 +1,26 @@
+%% Copyright (C) 2022 - Luca Novarini
+%% 
+%% This program is free software: you can redistribute it and/or modify it under
+%% the terms of the GNU General Public License as published by the Free Software
+%% Foundation, either version 3 of the License, or (at your option) any later
+%% version.
+%% 
+%% This program is distributed in the hope that it will be useful, but WITHOUT 
+%% ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+%% FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+%% details.
+%% 
+%% You should have received a copy of the GNU General Public License along with 
+%% this program. If not, see <https://www.gnu.org/licenses/>.
+%%
+%% usage: [ALPHA] = fcn_0To90TimeConstantToAlpha(TIME, FS, BLOCKSIZE)
+%%
+%% Calculates the correct ALPHA value for exponential smoothing filter (1st 
+%% order low-pass) to obtain a step response going from 0% to 90% of 
+%% steady-state final level in given TIME.
+%% FS is the sample frequency in Hz, BLOCKSIZE is the number of samples in each
+%% frame or 1 when working sample-by-sample.
+
+function [ALPHA] = fcn_0To90TimeConstantToAlpha(TIME, FS, BLOCKSIZE)
+    ALPHA = 0.1 ^ (BLOCKSIZE / (TIME * FS));
+end
---
+++ fcn_ExpSmoothing.m
@@ -0,0 +1,29 @@
+%% Copyright (C) 2022 - Luca Novarini
+%% 
+%% This program is free software: you can redistribute it and/or modify it under
+%% the terms of the GNU General Public License as published by the Free Software
+%% Foundation, either version 3 of the License, or (at your option) any later
+%% version.
+%% 
+%% This program is distributed in the hope that it will be useful, but WITHOUT 
+%% ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+%% FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+%% details.
+%% 
+%% You should have received a copy of the GNU General Public License along with 
+%% this program. If not, see <https://www.gnu.org/licenses/>.
+%%
+%% usage: [Y_N] = fcn_ExpSmoothing(X_N, Y_NMINUSONE, ALPHA)
+%%
+%% Calculates current output sample for an exponential smoothing filter (1st 
+%% order Low-Pass filter) according the following differential equation:
+%%
+%% y[n] = alpha * y[n-1] + (1 - alpha) * x[n]
+%%
+%% where current output sample y[n] is Y_N, smoothing time constant alpha is 
+%% ALPHA, previous output sample y[n-1] is Y_NMINUSONE, and current input 
+%% sample x[n] is X_N.
+
+function [Y_N] = fcn_ExpSmoothing(X_N, Y_NMINUSONE, ALPHA)
+	Y_N = X_N + ALPHA * (Y_NMINUSONE - X_N);
+end
