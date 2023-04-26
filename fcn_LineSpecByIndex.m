1f07e31e537a023a555c99a512c8c26f312b809c
LuchkaOnTheMoon <110480465+LuchkaOnTheMoon@users.noreply.github.com>
2023-04-26 12:15:52 +0200

    Update fcn_LineSpecByIndex.m

--- fcn_LineSpecByIndex.m
+++ fcn_LineSpecByIndex.m
@@ -1,4 +1,4 @@
-%% Copyright (C) 2022 - Luca Novarini
+%% Copyright (C) 2022 - Luchika De Sousa
 %% 
 %% This program is free software: you can redistribute it and/or modify it under
 %% the terms of the GNU General Public License as published by the Free Software
